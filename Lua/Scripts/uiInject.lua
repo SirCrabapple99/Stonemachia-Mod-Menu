5d799eef987499ca6d098733505bb89575cb8ee4
SirCrabapple99 <99812880+SirCrabapple99@users.noreply.github.com>
2026-08-15 15:48:55 -0500

    Mods button auto injects and also now has an onclick function registered

--- Lua/Scripts/uiInject.lua
+++ Lua/Scripts/uiInject.lua
@@ -2,34 +2,41 @@ local bpLib = StaticFindObject("/Script/UMG.Default__WidgetBlueprintLibrary")
 
 local uiInject = {}
 
-function uiInject.injectButtonVertical(ownerClassName, menuName, buttonName, insertIndex)
-    -- find all the buttons in the menu chosen
-    local all = FindAllOf(menuName)
-    if not all then
-        print("[ModMenu] [uiInject] [injectButtonVertical] menu with name of " .. menuName .. " not found")
+-- dump widget tree
+local panelClass = StaticFindObject("/Script/UMG.PanelWidget")
+local userWidgetClass = StaticFindObject("/Script/UMG.UserWidget")
+
+local function dumpTree(widget, depth)
+    if not widget or not widget:IsValid() then
+        return
+    end
+    print(string.rep("  ", depth) .. widget:GetFName():ToString() .. "  [" .. widget:GetClass():GetFName():ToString() ..
+              "]\n")
+
+    -- walk walk walk
+    if widget:IsA(panelClass) then
+        for i = 0, widget:GetChildrenCount() - 1 do
+            dumpTree(widget:GetChildAt(i), depth + 1)
+        end
+        -- walk walk walk
+    elseif widget:IsA(userWidgetClass) then
+        local t = widget.WidgetTree
+        if t and t:IsValid() then
+            dumpTree(t.RootWidget, depth + 1)
+        end
+    end
+end
+
+function uiInject.injectButtonVertical(menu, buttonName, insertIndex)
+    if not menu or not menu:IsValid() then
+        print("[ModMenu] [uiInject] [injectButtonVertical] provided menu is invalid\n")
         return
     end
 
-    -- find the selected button
-    local templateInstance = nil
-    for unused, widget in ipairs(all) do
-        -- if buttons found
-        if widget:GetFName():ToString() == buttonName then
-            -- get outer
-            local outer = widget:GetOuter()
-            if outer then
-                -- get outer outer
-                local owner = outer:GetOuter()
-                -- proceed
-                if owner and owner:IsValid() and owner:GetClass():GetFName():ToString() == ownerClassName then
-                    templateInstance = widget
-                end
-            end
-        end
-    end
-
-    if not templateInstance then
-        print("[ModMenu] [uiInject] [injectButtonVertical] button with name of " .. buttonName .. " not found")
+    -- get the selected button from the name
+    local templateInstance = menu[buttonName]
+    if not templateInstance or not templateInstance:IsValid() then
+        print("[ModMenu] [uiInject] " .. buttonName .. " not found in " .. menu:GetFullName() .. "\n")
         return
     end
 
@@ -39,14 +46,14 @@ function uiInject.injectButtonVertical(ownerClassName, menuName, buttonName, ins
     -- now get parent of the settings button
     local verticalBox = templateInstance:GetParent()
     if not verticalBox or not verticalBox:IsValid() then
-        print("[ModMenu] [uiInject] [injectButtonVertical] menu does not contain a VerticalBox")
+        print("[ModMenu] [uiInject] [injectButtonVertical] menu does not contain a VerticalBox\n")
         return
     end
 
     -- get the player controller
-    local playerController = FindFirstOf("PlayerController")
+    local playerController = menu:GetOwningPlayer()
     if not playerController or not playerController:IsValid() then
-        print("[ModMenu] [uiInject] [injectButtonVertical] unable to find a PlayerController")
+        print("[ModMenu] [uiInject] [injectButtonVertical] unable to find a PlayerController\n")
         return
     end
 
@@ -59,7 +66,7 @@ function uiInject.injectButtonVertical(ownerClassName, menuName, buttonName, ins
     -- then add the new button into the list
     local slot = verticalBox:AddChildToVerticalBox(newButton)
     if not slot then
-        print("[ModMenu] [uiInject] [injectButtonVertical] unable inject new button into the menu")
+        print("[ModMenu] [uiInject] [injectButtonVertical] unable inject new button into the menu\n")
         return
     end
 
@@ -68,37 +75,181 @@ function uiInject.injectButtonVertical(ownerClassName, menuName, buttonName, ins
         return slot
     end
 
-    -- save all children
+    -- save all children along with their slot properties since ClearChildren destroys them
     local children = {}
     local childCount = verticalBox:GetChildrenCount()
 
     for i = 0, childCount - 1 do
-        children[#children + 1] = verticalBox:GetChildAt(i)
+        local child = verticalBox:GetChildAt(i)
+        local s = child.Slot
+        children[#children + 1] = {
+            widget = child,
+            padding = {
+                Left = s.Padding.Left,
+                Top = s.Padding.Top,
+                Right = s.Padding.Right,
+                Bottom = s.Padding.Bottom
+            },
+            hAlign = s.HorizontalAlignment,
+            vAlign = s.VerticalAlignment,
+            rule = s.Size.SizeRule,
+            value = s.Size.Value
+        }
     end
+
     -- remove the new button from the end
-    table.remove(children)
+    local moved = table.remove(children)
 
     -- insert it into the children index
-    table.insert(children, insertIndex, newButton)
+    if insertIndex > #children + 1 then
+        insertIndex = #children + 1
+    end
+    table.insert(children, insertIndex, moved)
 
     -- remove all children from the vertical box
     verticalBox:ClearChildren()
-    -- add all children back in the correct order
-    for unused, widget in ipairs(children) do
-        verticalBox:AddChildToVerticalBox(widget)
+
+    -- add all children back in the correct order and restore their slots
+    local newSlot
+    for i, entry in ipairs(children) do
+        local s = verticalBox:AddChildToVerticalBox(entry.widget)
+        s:SetPadding(entry.padding)
+        s:SetHorizontalAlignment(entry.hAlign)
+        s:SetVerticalAlignment(entry.vAlign)
+        s.Size.SizeRule = entry.rule
+        s.Size.Value = entry.value
+        if i == insertIndex then
+            newSlot = s
+        end
     end
-    
-    return slot
+
+    return newSlot, newButton
 end
 
-RegisterKeyBind(Key.F9, function()
-    local slot = uiInject.injectButtonVertical("WBP_PauseMenùVoices_C", "WBP_MainMenùButton_C", "BSettings", 6)
-    slot:SetPadding({
-        Left = 0,
-        Top = 4,
-        Right = 0,
-        Bottom = 4
-    })
+-- store if injected or not so it doesn't create a new button every time
+local injected = {}
+-- track the clones that were created so the click hook can filter to the right one
+local myButtons = {}
+
+-- register hook on pause menu creation
+RegisterHook("/Game/Widget/PauseMenù/Pages/WBP_PauseMenùVoices.WBP_PauseMenùVoices_C:Construct", function(ctx)
+    local menu = ctx:get()
+    if not menu or not menu:IsValid() then
+        print("[ModMenu] [uiInject] [pauseMenuHook] menu returned nil")
+    end
+
+    -- guard against creating a second button
+    local id = menu:GetFullName()
+    local button = injected[id]
+
+    if not button or not button:IsValid() then
+        local slot
+        slot, button = uiInject.injectButtonVertical(menu, "BSettings", 8)
+        if not slot or not button then
+            print("[ModMenu] [uiInject] [pauseMenuHook] injection failed\n")
+            return
+        end
+
+        slot:SetPadding({
+            Left = 0,
+            Top = 4,
+            Right = 0,
+            Bottom = 4
+        })
+
+        -- add button to injected and also to the list of buttons for clicking
+        injected[id] = button
+        myButtons[button:GetFullName()] = true
+    end
+
+    -- need to use GetChildAt for everything because the letter ù creates errors
+    local root = button.WidgetTree.RootWidget
+    local uButton = root:GetChildAt(0)
+    local vbox = uButton:GetChildAt(0)
+    local title = vbox:GetChildAt(0)
+
+    -- set text every time or it will revert to default
+    title:SetText(FText("Mods"))
 end)
+
+-- register hook on main menu creation
+RegisterHook("/Game/Widget/MainMenù/Componenets/Pages/WBP_MainPage.WBP_MainPage_C:Construct", function(ctx)
+    local menu = ctx:get()
+    if not menu or not menu:IsValid() then
+        print("[ModMenu] [uiInject] [mainMenuHook] menu returned nil")
+    end
+
+    -- guard against creating a second button
+    local id = menu:GetFullName()
+    local button = injected[id]
+
+    if not button or not button:IsValid() then
+        local slot
+        slot, button = uiInject.injectButtonVertical(menu, "BSettings", 6)
+        if not slot or not button then
+            print("[ModMenu] [uiInject] [mainMenuHook] injection failed\n")
+            return
+        end
+        slot:SetPadding({
+            Left = 0,
+            Top = 4,
+            Right = 0,
+            Bottom = 4
+        })
+
+        -- add button to injected and also to the list of buttons for clicking
+        injected[id] = button
+        myButtons[button:GetFullName()] = true
+    end
+
+    -- need to use GetChildAt for everything because the letter ù creates errors
+    local root = button.WidgetTree.RootWidget
+    local uButton = root:GetChildAt(0)
+    local vbox = uButton:GetChildAt(0)
+    local title = vbox:GetChildAt(0)
+
+    -- set text every time or it will revert to default
+    title:SetText(FText("Mods"))
+end)
+
+-- now for the clicking stuff
+
+-- register the click hook once at load off the button class itself
+local buttonCls = StaticFindObject(
+    "/Game/Widget/MainMen\u{F9}/Componenets/Button/WBP_MainMen\u{F9}Button.WBP_MainMen\u{F9}Button_C")
+if not buttonCls or not buttonCls:IsValid() then
+    print("[ModMenu] [uiInject] button class not loaded, click hook not registered\n")
+else
+    -- the OnRelease function has to be found again because of the letter ù
+    local clickFn
+    buttonCls:ForEachFunction(function(fn)
+        local n = fn:GetFName():ToString()
+        if n:find("BndEvt", 1, true) and n:find("OnButtonReleasedEvent", 1, true) then
+            clickFn = n
+        end
+    end)
+
+    if not clickFn then
+        print("[ModMenu] [uiInject] [clickingStuff] OnRelease function could not be found\n")
+        return
+    end
+
+    -- find the full assetpath and then register a hook to it + the OnRelease function name (I know this is bad but it works)
+    local assetPath = buttonCls:GetFullName():match("%S+%s+(.+)")
+    RegisterHook(assetPath .. ":" .. clickFn, function(ctx)
+        local self = ctx:get()
+        if not self or not self:IsValid() then
+            return
+        end
+        if not myButtons[self:GetFullName()] then
+            return
+        end
+
+        -- function stuff goes here (probably just call a function to enable bp menu)
+        for i = 0, 10 do
+            print("[ModMenu] MODS BUTTON CLICKED YAY IT WORKS\n")
+        end
+    end)
+end
 
 return uiInject
