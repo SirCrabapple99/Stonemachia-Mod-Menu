a1420b6b52c4070e57254724f88aecae7b91090a
Evryon <fireden70@gmail.com>
2026-08-15 19:30:25 +0200

    Added status check function in modstxt_parser and added debug cli

--- Lua/Scripts/main.lua
+++ Lua/Scripts/main.lua
@@ -3,9 +3,9 @@ local DEBUG = true
 local scraped_mods = require("mods scraper")
 local modstxt_parser = require("modstxt parser")
 modstxt_parser.validate_mod_entries(scraped_mods)
+if DEBUG then require("cli")(scraped_mods) end
 
 local uiInject = require("uiInject")
 RegisterKeyBind(Key.F9, function()
     uiInject.injectButtonVertical("WBP_MainPage_C", "WBP_MainMenùButton_C", "BSettings"):SetPadding({Left = 0, Top = 4, Right = 0, Bottom = 4})
 end)
-
