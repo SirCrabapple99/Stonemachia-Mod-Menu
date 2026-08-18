119f1848a8e74ae015162412e0afe66908647737
SirCrabapple99 <99812880+SirCrabapple99@users.noreply.github.com>
2026-08-18 16:47:24 -0500

    pray

--- Lua/Scripts/bpInterface.lua
+++ Lua/Scripts/bpInterface.lua
@@ -1,25 +1,58 @@
 local BP = {}
 
--- register a custom event for each widget so we can get it as a reference
-local canvas
-RegisterCustomEvent("ModMenuCanvasSpawn", function(self)
+-- register a custom event for each widget so we can have it as a reference
+local ModMenu
+local PC
+RegisterCustomEvent("ModMenuSpawn", function(self)
     local widget = self:get()
     if widget and widget:IsValid() then
-        canvas = widget
-        print("[ModMenu] Canvas registered\n")
+        ModMenu = widget
+        print("[ModMenu] ModMenu registered\n")
+    end
+
+    local pcMaster = FindFirstOf("PC_PlayerControllerMaster_C")
+    if pcMaster and pcMaster:IsValid() then
+        PC = pcMaster
+    end
+end)
+
+local vertbox
+RegisterCustomEvent("ModMenuVertBoxSpawn", function(self)
+    local widget = self:get()
+    if widget and widget:IsValid() then
+        vertbox = widget
+        print("[ModMenu] VertBox registered\n")
     end
 end)
 
 -- self explanatory
-function BP.Canvas_ToggleVisibility()
+function BP.ModMenu_ToggleVisibility()
     ExecuteInGameThread(function()
-        canvas:ToggleVisibility()
+        ModMenu:ToggleVisibility()
+    end)
+end
+
+function BP.ModMenu_FocusInput()
+    ExecuteInGameThread(function()
+        PC:OnWidgetOpened(ModMenu, false, false)
+    end)
+end
+
+function BP.ModMenu_UnfocusInput()
+    ExecuteInGameThread(function()
+        PC:OnWidgedClosed()
     end)
 end
 
 RegisterKeyBind(Key.O, function()
-    BP.Canvas_ToggleVisibility()
+    BP.ModMenu_ToggleVisibility()
 end)
+
+RegisterKeyBind(Key.R, function()
+    BP.ModMenu_FocusInput()
+end)
+
+RegisterKeyBind(Key.T, function() BP.ModMenu_Unfocus() end)
 
 return BP
 
